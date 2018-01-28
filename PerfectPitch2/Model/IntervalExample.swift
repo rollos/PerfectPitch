382c3e948e861332a6850c1c2e80e9a718448d6a
rollos <woodymelling@gmail.com>
2018-01-28 15:48:08 -0700

    initial commit

---
+++ PerfectPitch2/Model/IntervalExample.swift
@@ -0,0 +1,24 @@
+//
+//  IntervalExample.swift
+//  PerfectPitch2
+//
+//  Created by Woodrow Melling on 1/26/18.
+//  Copyright © 2018 Woodrow Melling. All rights reserved.
+//
+
+import Foundation
+
+class IntervalExample {
+    
+    let helpText: String
+    let interval: String
+    let note: String
+    
+    init(songText:String, intervalName:String, noteName:String){
+        helpText = songText
+        interval = intervalName
+        note = noteName
+    }
+    
+    
+}
